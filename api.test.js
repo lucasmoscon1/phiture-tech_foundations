553f2c7b5f38053708ca9c84926ba7375235d10f
lucasmoscon1 <145936676+lucasmoscon1@users.noreply.github.com>
2024-12-16 20:04:58 -0300

    Create api.test.js

---
+++ api.test.js
@@ -0,0 +1,20 @@
+const app = require('./index.js')
+const supertest = require('supertest')
+const request = supertest(app)
+
+test('testing if jest works', () => {
+    expect(1).toBe(1)
+})
+
+test('Testing root endpoint', async () => {
+    const res = await request.get('/')
+    const message = res.body.message
+    expect(message).toBe('hi!!')
+})
+
+
+test('Testing recommend endpoint', async () => {
+    const res = await request.get('/recommend').query({'type':'fruits'})
+    const message = res.body.recommendation
+    expect(message).toBe('Apple')
+})
