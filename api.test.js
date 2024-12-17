d82fb8c5d7ad8b2f80c1c9c0b687afb09d1c7149
lucasmoscon1 <145936676+lucasmoscon1@users.noreply.github.com>
2024-12-16 23:05:50 -0300

    Update api.test.js

--- api.test.js
+++ api.test.js
@@ -3,11 +3,11 @@ const supertest = require('supertest')
 const request = supertest(app)
 
 test('testing if jest works', () => {
-    expect(1).toBe(1)
+    expect(1).toBe(20)
 })
 
 test('Testing root endpoint', async () => {
     const res = await request.get('/')
     const message = res.body.message
-    expect(message).toBe('hi!!')
+    expect(message).toBe('hi!')
 })
